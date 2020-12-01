17782519d627713e36a38336ef043fc51f22f300
Sondre Kaavik <nep012@student.uib.no>
2020-12-01 13:52:26 +0100

    fin dag1

--- dag1know.hs
+++ dag1know.hs
@@ -3,4 +3,4 @@ import System.IO
 main = do 
 	contents <- readFile "input1know.txt"
 	y <- readIO contents :: IO [Int]
-	putStrLn $ show ([x | x <- [1..100000], x `notElem` y])
+	putStrLn $ show $ filter (\x -> x `notElem` y) [1..100000]
